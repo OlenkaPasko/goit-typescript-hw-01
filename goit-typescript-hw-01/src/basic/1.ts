a5afeb81ba386b572fb131b57fea8878c4fbb96c
OlenkaPasko <113981072+OlenkaPasko@users.noreply.github.com>
2024-08-16 16:19:08 +0300

    1.ts

---
+++ goit-typescript-hw-01/src/basic/1.ts
@@ -0,0 +1,7 @@
+import "./style.css";
+
+const age: number = 50;
+const username: string = 'Max';
+const toggle: boolean = true;
+const empty: null = null;
+const callback = (a:number):number => { return 100 + a };
